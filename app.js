28dfdce9da575c9bb3d4bda978ba5ecd2acd7529
Joaquin Kaplan <93784263+joaquinkaplan@users.noreply.github.com>
2022-01-24 13:55:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,9 +9,11 @@ const icon = document.querySelector('.icon img');
 
 
 const updateUI = (data) => {
+
     console.log(data);
     /*const cityDets = data.cityDets;
     const weather = data.weather;*/
+
 
     //destructure properties (the same as above)
     const { cityDets, weather } = data;
@@ -26,47 +28,78 @@ const updateUI = (data) => {
 </div>
 `;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     //update the night/day & icon images
-    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`
-    icon.setAttribute('src', iconSrc);
-    let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
-    //the same as above    
-    /*let timeSrc = null;
-        if(weather.IsDayTime){
-            timeSrc = 'img/day.svg';
-        } else {
-            timeSrc = 'img/night.svg';
-        }*/
+const iconSrc = `img/icons/${weather.WeatherIcon}.svg`
+icon.setAttribute('src', iconSrc);
+
+
+
+let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+
+//the same as above    
+/*let timeSrc = null;
+    if(weather.IsDayTime){
+        timeSrc = 'img/day.svg';
+    } else {
+        timeSrc = 'img/night.svg';
+    }*/
 
     time.setAttribute('src', timeSrc);
+  
+
 
     // remove the d-none class if present
     if (card.classList.contains('d-none')) {
         card.classList.remove('d-none');
     }
+
+
+
+
 };
 
 
-
 const updateCity = async (city) => {
+
     const cityDets = await getCity(city);
     const weather = await getWeather(cityDets.Key);
+
     return {
         //object shorthand notation. the same as saying cityDets: cityDets   or weather: weather
         cityDets,
         weather
     };
+
 };
 
 
 cityForm.addEventListener('submit', e => {
     //prevent default action
     e.preventDefault();
+
+
     //get city value
     const city = cityForm.city.value.trim();
     cityForm.reset();
+
     //update the ui with new city
     updateCity(city)
         .then(data => updateUI(data))
         .catch(err => console.log(err));
+
+
 });
